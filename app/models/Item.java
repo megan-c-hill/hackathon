0f42822ef1718120f410dd4be87177f4ceb9f2a1
megan-c-hill <meganchill@iastate.edu>
2017-10-20 21:04:41 -0500

    Added basic class and HTML page

---
+++ app/models/Item.java
@@ -0,0 +1,31 @@
+package models;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+import java.time.LocalDateTime;
+
+public class Item {
+    private String name;
+    private LocalDateTime expiration;
+    private boolean expired;
+
+    public Item(String name, LocalDateTime expiration){
+        this.name = name;
+        this.expiration = expiration;
+        setExpired
+    }
+
+    private void setExpired(){
+        LocalDateTime now = LocalDateTime.now();
+        if (expiration.isBefore(now)){
+            expired = false;
+        }
+        else{
+            expired = true;
+        }
+    }
+}
